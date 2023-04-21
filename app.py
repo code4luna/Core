100e5538b82a2558e8fb812a6eead63276dd86ce
Christopher <115739447+code4luna@users.noreply.github.com>
2023-04-21 13:30:30 -0600

    Added a homepage to showcase uploaded files

--- app.py
+++ app.py
@@ -1,22 +1,46 @@
 from flask import Flask, render_template, request
-from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask_dropzone import Dropzone
+import os
+from werkzeug.utils import secure_filename
+from flask_login import UserMixin
 
 app = Flask(__name__)
+dropzone = Dropzone(app)
 
-photos = UploadSet('photos', IMAGES)
-app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'
-configure_uploads(app, photos)
+app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*, video/*'
+app.config['DROPZONE_MAX_FILE_SIZE'] = 100  # Set max file size limit (in MB)
+app.config['DROPZONE_MAX_FILES'] = 10
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    file_urls = [f'/static/uploads/{f}' for f in files]
+    return render_template('index.html', file_urls=file_urls)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        filename = photos.save(request.files['photo'])
-        return f"File '{filename}' uploaded successfully!"
+    if request.method == 'POST':
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return "Files uploaded successfully!", 204
     return render_template('upload.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+users = {
+    "user1": User("user1", "user1@example.com", "password1"),
+    "user2": User("user2", "user2@example.com", "password2"),
+}
